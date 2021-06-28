62a6ef361b7e5c9b243f1f5a935056bc1a6fa5fa
Tom Parks <tmparks@yahoo.com>
2021-06-28 08:44:41 -0400

    use image title as link

--- functions.php
+++ functions.php
@@ -1,9 +1,21 @@
 <?php
 
-// Add support for featured images.
+/// Add support for featured images.
+/// See [Featured Images and Post Thumbnails](https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/)
 add_action('after_setup_theme', function () {
   add_theme_support('post-thumbnails');
 });
+
+/// Each featured image uses its title as a link.
+add_filter(
+  'post_thumbnail_html',
+  function($html, $post_id, $post_image_id) {
+    $title = get_the_title($post_image_id );
+    if ($title != '') {
+      $html = '<a href="' . esc_attr($title) . '">' . $html . '</a>';
+    }
+    return $html;
+}, 10, 3);
 
 /// Sort posts by name (slug) in ascending order.
 add_action('pre_get_posts', function($query) {
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<div class="tile"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a><br/><?php the_title(); ?></div>
+<div class="tile"><?php the_post_thumbnail(); ?><br/><?php the_title(); ?></div>
 <?php endwhile; ?>
 <?php else : ?>
 <?php _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); ?>
