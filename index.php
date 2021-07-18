fbe23198100e50c7b791e02f48d8608b132dfc4a
Tom Parks <tmparks@yahoo.com>
2021-07-18 16:33:22 -0400

    add basic instructions

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
+
+<h1>Categories</h1>
 <?php echo get_the_category_list(); ?>
+
+<h1>Geting Started</h1>
+<p>This theme displays a grid of icons.</p>
+<p>Create posts, one for each icon. The title of a post is displayed below its icon.</p>
+<p>Upload a featured image for each post. The featured image is used as the icon for a post.</p>
+<p>Set a title for each featured image. The title is used as a link when the icon is tapped.</p>
+
+<h1>Advanced Options</h1>
+<p>Navigate to <strong>Appearance > Customize > Site Identity</strong> and upload a site icon.</p>
+<p>Assign each post to one or more categories. A grid is created for each category.</p>
+
 <?php get_footer(); ?>
